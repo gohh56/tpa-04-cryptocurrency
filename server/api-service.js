c8e97a1ce1a84c010f800b90c296bc9a2119fb60
gohh56 <ystokio@gmail.com>
2019-01-30 02:08:04 +0900

    to components

--- server/api-service.js
+++ server/api-service.js
@@ -1,4 +1,6 @@
 const getHistoricalData = function() {
+  //https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=JPY&api_key=20ecd2849b8b3975d1202e6d7675737abac87b694ad665c6036b89858ed6cfdd
+  //@return { "JPY": nnnnn.nn }
   return Promise.resolve({});
 };
 
