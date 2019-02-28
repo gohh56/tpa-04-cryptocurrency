a0c7f8e823eb151bc997c5fa63404fd23cd8065c
gohh56 <ystokio@gmail.com>
2019-03-01 00:14:03 +1000

    fixed api-service.test.js

--- server/api-service.test.js
+++ server/api-service.test.js
@@ -1,32 +1,35 @@
 const { getHistoricalData, getMarketInformation } = require('./api-service');
 
 describe('getHistoricalData', () => {
-  test('should return an object', async() => {
-    const historicalData = await getHistoricalData('BTC', 'JPY');
+  let historicalData;
+  beforeAll(async() => {
+    historicalData = await getHistoricalData('BTC', 'JPY');
+  });
+  test('should return an object', () => {
     expect(historicalData).toBeDefined();
     expect(typeof historicalData).toBe('object');
   });
-  test('should have correct properties', async() => {
-    const historicalData = await getHistoricalData('BTC', 'JPY');
+  test('should have correct properties', () => {
     expect(historicalData[0]).toHaveProperty('time');
     expect(historicalData[0]).toHaveProperty('close');
     expect(historicalData[0]).toHaveProperty('high');
     expect(historicalData[0]).toHaveProperty('low');
   });
-  test('should have 1440 chartData elements', async() => {
-    const historicalData = await getHistoricalData('BTC', 'JPY');
+  test('should have 1440 chartData elements', () => {
     expect(historicalData).toHaveLength(1441);
   });
 });
 
 describe('getMarketInformation', () => {
-  test('should return an object', async() => {
-    const marketInfomation = await getMarketInformation('BTC', 'JPY');
+  let marketInfomation;
+  beforeAll(async() => {
+    marketInfomation = await getMarketInformation('BTC', 'JPY');
+  });
+  test('should return an object', () => {
     expect(marketInfomation).toBeDefined();
     expect(typeof marketInfomation).toBe('object');
   });
-  test('should have correct properties', async() => {
-    const marketInfomation = await getMarketInformation('BTC', 'JPY');
+  test('should have correct properties', () => {
     expect(marketInfomation).toHaveProperty('volume_24h');
     expect(marketInfomation).toHaveProperty('market_cap');
   });
